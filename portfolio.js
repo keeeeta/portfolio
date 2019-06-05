a58d76f5cac6de20c1b77b782910edcf2f94ca95
keeeeta <51400779+keeeeta@users.noreply.github.com>
2019-06-05 18:02:17 +0900

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+// $(window).load(function () {
+//     $('#preload').fadeOut();
+// });
+
+// $(window).scroll(function () {
+//     console.log($(this).scrollTop());
+// })
+
+$(function () {
+    $('span').css('transform', 'scale(3)');
+    $('#preload').delay(2000).fadeOut(3000);
+});
